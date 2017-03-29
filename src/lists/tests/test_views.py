dda406bcf3970573424b9247bdf47cc2bdce9778
lttviet <lttviet@gmail.com>
2017-03-28 22:40:58 -0400

    Split out unit tests into 2 files

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -13,36 +13,6 @@ class HomePageTest(StaticLiveServerTestCase):
         self.assertTemplateUsed(response, "home.html")
 
 
-class ListAndItemModelTest(StaticLiveServerTestCase):
-
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = "The 1st item"
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "2nd item"
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The 1st item")
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, "2nd item")
-        self.assertEqual(second_saved_item.list, list_)
-
-
 class ListViewTest(StaticLiveServerTestCase):
 
     def test_uses_list_template(self):
