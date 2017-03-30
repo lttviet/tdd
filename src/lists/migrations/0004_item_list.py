38b4b57e49381f3bdd13c3218f925df5e3a76979
lttviet <lttviet@gmail.com>
2017-03-30 19:40:30 -0400

    Implement duplicate item validation at model layer

--- src/lists/migrations/0004_item_list.py
+++ src/lists/migrations/0004_item_list.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -15,6 +16,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='item',
             name='list',
-            field=models.TextField(default=''),
+            field=models.ForeignKey(default=None, on_delete=django.db.models.deletion.CASCADE, to='lists.List'),
         ),
     ]
