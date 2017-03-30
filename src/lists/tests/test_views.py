eb671b15cf1cbe2476aaf917b61ce2ac5779d49c
lttviet <lttviet@gmail.com>
2017-03-29 20:27:31 -0400

    Refactor list view to handle new item POSTs

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -69,15 +69,12 @@ class NewListTest(StaticLiveServerTestCase):
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
 
-
-class NewItemTest(StaticLiveServerTestCase):
-
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
-                "/lists/{}/add_item".format(correct_list.id),
+                f'/lists/{correct_list.id}/',
                 data={"item_text": "A new item for existing list"}
                 )
 
@@ -86,13 +83,13 @@ class NewItemTest(StaticLiveServerTestCase):
         self.assertEqual(new_item.text, "A new item for existing list")
         self.assertEqual(new_item.list, correct_list)
 
-    def test_redirects_to_list_view(self):
+    def test_POST_redirects_to_list_view(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
 
         response = self.client.post(
-                "/lists/{}/add_item".format(correct_list.id),
+                f'/lists/{correct_list.id}/',
                 data={"item_text": "A new item for existing list"}
                 )
 
-        self.assertRedirects(response, "/lists/{}/".format(correct_list.id))
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
