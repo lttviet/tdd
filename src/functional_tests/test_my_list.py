aa96d6691e80c21cf4715a2b5bec8b7745266549
lttviet <lttviet@gmail.com>
2017-04-07 18:25:47 -0400

    add test

--- src/functional_tests/test_my_list.py
+++ src/functional_tests/test_my_list.py
@@ -3,13 +3,15 @@ from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
 from django.contrib.sessions.backends.db import SessionStore
 from .base import FunctionalTest
 
+User = get_user_model()
+
 class MyListTest(FunctionalTest):
 
-    def create_pre_authenticated_session(self):
+    def create_pre_authenticated_session(self, email):
         user = User.objects.create(email=email)
         session = SessionStore()
         session[SESSION_KEY] = user.pk
-        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKEND[0]
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
         session.save()
 
         self.browser.get(self.live_server_url + '/404_no_such_url/')
@@ -18,3 +20,12 @@ class MyListTest(FunctionalTest):
             value=session.session_key,
             path='/'
         ))
+
+    def test_logged_in_users_lists_are_saved_as_my_list(self):
+        email = 'a@b.com'
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_out(email)
+
+        self.create_pre_authenticated_session(email)
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_in(email)
