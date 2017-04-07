c404fe369156a16aa7e38b8f6c6d6686ef58f80b
lttviet <lttviet@gmail.com>
2017-04-07 16:38:28 -0400

    update urls

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.core.mail import send_mail
+from django.shortcuts import redirect
+from django.contrib import messages, auth
+
+from accounts.models import Token
 
 # Create your views here.
+def send_login_email(request):
+    email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to login:\n\n{url}'
+    send_mail(
+        'Your login link for tdd',
+        message_body,
+        'noreply@lttviet.com',
+        [email]
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to login"
+    )
+    return redirect('/')
+
+def login(request):
+    user = auth.authenticate(uid=request.GET.get('token'))
+    if user:
+        auth.login(request, user)
+    return redirect('/')
