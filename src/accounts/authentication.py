fa572c413ec532203c6a7bea9c40c3a3babb29f6
lttviet <lttviet@gmail.com>
2017-04-07 16:37:37 -0400

    add passwordless auth backend

---
+++ src/accounts/authentication.py
@@ -0,0 +1,18 @@
+from accounts.models import Token, User
+
+class PasswordlessAuthenticationBackend():
+
+    def authenticate(self, uid):
+        try:
+            token = Token.objects.get(uid=uid)
+            return User.objects.get(email=token.email)
+        except Token.DoesNotExist:
+            return
+        except User.DoesNotExist:
+            return User.objects.create(email=token.email)
+
+    def get_user(self, email):
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            return
